2128d286cae05342051a563f4a0ad557e9d7b9e6
dilmurod khamidov <dilmuroddelitot@gmail.com>
2023-05-04 12:50:51 +0500

    Header

--- client/src/pages/Auth/login.js
+++ client/src/pages/Auth/login.js
@@ -48,7 +48,7 @@ const Login = () => {
             onChange={(e) => SetEmail(e.target.value)}
             type="email"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-4"
-            placeholder="Your name"
+            placeholder="Your email"
             required
           />
           <input
@@ -56,7 +56,7 @@ const Login = () => {
             onChange={(e) => SetPassword(e.target.value)}
             type="password"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-4"
-            placeholder="Your name"
+            placeholder="Your password"
             required
           />
           <button
